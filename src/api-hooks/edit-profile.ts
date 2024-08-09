48d29625be59a150e08a181ff633dfe1b69008eb
Ghazal Moghadam <moghadam.ghazal@gmail.com>
2024-08-10 02:12:12 +0330

    fix build error

--- src/api-hooks/edit-profile.ts
+++ src/api-hooks/edit-profile.ts
@@ -1,6 +1,5 @@
 import { useMutation, useQuery } from "@tanstack/react-query";
 import { useHttpClient } from "../common/http-client";
-import { SignupFormValue } from "../common/types/sign-up";
 import { HTTPError } from "ky";
 import { useNavigate } from "react-router-dom";
 import { urls } from "../common/routes";
--- src/route-components/edit-profile.tsx
+++ src/route-components/edit-profile.tsx
@@ -1,109 +1,109 @@
-import { SubmitHandler, useForm } from "react-hook-form";
-import { zodResolver } from "@hookform/resolvers/zod";
-import {
-  SignupFormValue,
-  signupFormValueSchema,
-} from "../common/types/sign-up";
-import { Key } from "../assets/svg/key.tsx";
-import { InputField } from "../reusable-components/input-field";
-import { Envelope } from "../assets/svg/envelope.tsx";
-import { PersonIcon } from "../assets/svg/person-icon.tsx";
-import { SubmitBtn } from "../reusable-components/submit-btn.tsx";
-import { CancelBtn } from "../reusable-components/cancel-btn.tsx";
-import { urls } from "../common/routes.ts";
-import { useNavigate } from "react-router-dom";
-import { useEditProfile, useGetProfile } from "../api-hooks/edit-profile.ts";
-import { ProfileFormValue } from "../common/types/profile.ts";
+// import { SubmitHandler, useForm } from "react-hook-form";
+// import { zodResolver } from "@hookform/resolvers/zod";
+// import {
+//   SignupFormValue,
+//   signupFormValueSchema,
+// } from "../common/types/sign-up";
+// import { Key } from "../assets/svg/key.tsx";
+// import { InputField } from "../reusable-components/input-field";
+// import { Envelope } from "../assets/svg/envelope.tsx";
+// import { PersonIcon } from "../assets/svg/person-icon.tsx";
+// import { SubmitBtn } from "../reusable-components/submit-btn.tsx";
+// import { CancelBtn } from "../reusable-components/cancel-btn.tsx";
+// import { urls } from "../common/routes.ts";
+// import { useNavigate } from "react-router-dom";
+// import { useEditProfile, useGetProfile } from "../api-hooks/edit-profile.ts";
+// import { ProfileFormValue } from "../common/types/profile.ts";
 
- const EditProfileLayout = () => {
-    //remove export
-  const navigate = useNavigate();
-  const {data: profile } = useGetProfile();
-  const {
-    register,
-    handleSubmit,
-    formState: { errors },
-  } = useForm<SignupFormValue>({
-    criteriaMode: "all",
-    resolver: zodResolver(signupFormValueSchema),
-    // defaultValues: profile
-  });
+//  const EditProfileLayout = () => {
+//     //remove export
+//   const navigate = useNavigate();
+//   const {data: profile } = useGetProfile();
+//   const {
+//     register,
+//     handleSubmit,
+//     formState: { errors },
+//   } = useForm<SignupFormValue>({
+//     criteriaMode: "all",
+//     resolver: zodResolver(signupFormValueSchema),
+//     // defaultValues: profile
+//   });
 
-  //   const {data, isError, error:get}= useGetProfile();
-  //   useEffect(()=>{
-  //   },[]);
+//   //   const {data, isError, error:get}= useGetProfile();
+//   //   useEffect(()=>{
+//   //   },[]);
 
-    const { isError, error, mutate } = useEditProfile();
+//     const { isError, error, mutate } = useEditProfile();
 
-  const onSubmit: SubmitHandler<ProfileFormValue> = (formData) => {
-    mutate(formData);
-  };
+//   const onSubmit: SubmitHandler<ProfileFormValue> = (formData) => {
+//     mutate(formData);
+//   };
 
-//   if(!profile) {
-//     return <CircularProgress />
-//   }
+// //   if(!profile) {
+// //     return <CircularProgress />
+// //   }
 
-  return (
-    <form
-      className="flex flex-col gap-y-8 w-80 m-auto p-4"
-      onSubmit={handleSubmit(onSubmit)}
-    >
-      {/* {isError && <Alert severity="error">{error?.message}</Alert>} */}
-      <div dir="rtl">
-        <p className="text-xl text-center font-bold">
-          ویرایش حساب
-        </p>
+//   return (
+//     <form
+//       className="flex flex-col gap-y-8 w-80 m-auto p-4"
+//       onSubmit={handleSubmit(onSubmit)}
+//     >
+//       {/* {isError && <Alert severity="error">{error?.message}</Alert>} */}
+//       <div dir="rtl">
+//         <p className="text-xl text-center font-bold">
+//           ویرایش حساب
+//         </p>
 
-        <div className="flex flex-col gap-y-5">
-          <InputField
-            placeholder="نام"
-            defaultValue={profile?.name}
-            error={errors.username}
-            svg={PersonIcon}
-            {...register("username")}
-          />
-          <InputField
-            type="email"
-            placeholder="ایمیل"
-            defaultValue={profile?.email}
-            error={errors.email}
-            svg={Envelope}
-            {...register("email")}
-          />
-          <InputField
-            placeholder="رمز عبور"
-            defaultValue={profile?.password}
-            type="password"
-            error={errors.password}
-            svg={Key}
-            {...register("password")}
-          />
-          <InputField
-            placeholder="تکرار رمز عبور"
-            defaultValue={profile?.confirmPassword}
-            type="password"
-            error={errors.confirmPassword}
-            svg={Key}
-            {...register("confirmPassword")}
-          />
-        </div>
-      </div>
-      <div className="flex flex-row justify-end w-80 h-9 gap-2">
-        <CancelBtn onClick={() => navigate(urls.main + urls.myPage)}>
-          پشیمون شدم
-        </CancelBtn>
-        <SubmitBtn className="w-48" size="medium">
-          ثبت تغییرات
-        </SubmitBtn>
-      </div>
-    </form>
-  );
-};
+//         <div className="flex flex-col gap-y-5">
+//           <InputField
+//             placeholder="نام"
+//             defaultValue={profile?.name}
+//             error={errors.username}
+//             svg={PersonIcon}
+//             {...register("username")}
+//           />
+//           <InputField
+//             type="email"
+//             placeholder="ایمیل"
+//             defaultValue={profile?.email}
+//             error={errors.email}
+//             svg={Envelope}
+//             {...register("email")}
+//           />
+//           <InputField
+//             placeholder="رمز عبور"
+//             defaultValue={profile?.password}
+//             type="password"
+//             error={errors.password}
+//             svg={Key}
+//             {...register("password")}
+//           />
+//           <InputField
+//             placeholder="تکرار رمز عبور"
+//             defaultValue={profile?.confirmPassword}
+//             type="password"
+//             error={errors.confirmPassword}
+//             svg={Key}
+//             {...register("confirmPassword")}
+//           />
+//         </div>
+//       </div>
+//       <div className="flex flex-row justify-end w-80 h-9 gap-2">
+//         <CancelBtn onClick={() => navigate(urls.main + urls.myPage)}>
+//           پشیمون شدم
+//         </CancelBtn>
+//         <SubmitBtn className="w-48" size="medium">
+//           ثبت تغییرات
+//         </SubmitBtn>
+//       </div>
+//     </form>
+//   );
+// };
 
-export const EditProfile = () => {
-  return <EditProfileLayout />;
-};
+// export const EditProfile = () => {
+//   return <EditProfileLayout />;
+// };
 
-export const EditProfileMoblie = () => {
-  return <EditProfileLayout />;
-};
+// export const EditProfileMoblie = () => {
+//   return <EditProfileLayout />;
+// };
